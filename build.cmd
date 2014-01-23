349b6b032bd090413147ba3a4646c411cb40b507
Lev Gimelfarb <lev.gimelfarb@gmail.com>
2014-01-22 23:37:08 -0500

    Scripting pushing to NuGet

--- build.cmd
+++ build.cmd
@@ -1,5 +1,45 @@
 @echo off
-set VSCOMNTOOLS=%VS120COMNTOOLS%
-@call "%VSCOMNTOOLS%\VsDevCmd.bat"
 
-msbuild ProductionStackTrace.sln /p:Configuration=Release /t:Build
+for /f %%i in ('git rev-list HEAD --count') do set COMMIT_COUNT=%%i
+for /f %%i in ('git log -1 --format^=\"%%h\"') do set COMMIT_HASH=%%i
+for /f %%i in ('git status -s ^| find /C " "') do set UNCOMMITED_CHANGES=%%i
+for /f %%i in ('git rev-list origin/master..HEAD --count') do set UNPUSHED_COUNT=%%i
+
+if "%~1" == "deploy" goto :deploy
+
+:build
+set MSBUILDDIR=%ProgramFiles(x86)%\MSBuild\12.0\bin
+"%MSBUILDDIR%\msbuild" ProductionStackTrace.sln /t:Build /p:Configuration=Release /p:BuildPackageIfMarked=true ^
+    /p:VersionBuild=%COMMIT_COUNT%;VersionBuildMeta=%COMMIT_HASH% %MSBUILDPARAMS%
+
+exit /b
+
+:deploy
+if %UNCOMMITED_CHANGES% NEQ 0 goto :uncommited
+
+if not exist "%~dp0\_deploy" mkdir "%~dp0\_deploy"
+del /S /Q "%~dp0\_deploy\*.*"
+
+set MSBUILDPARAMS=/p:WriteNuGetVersionToFile=true "/p:PackageOutputDir=%~dp0\_deploy"
+call :build
+
+set /p VERSION=<_deploy\.version.txt
+if "%VERSION%"=="" goto :error
+git tag -m "Release v%VERSION%" "v%VERSION%"
+if errorlevel 1 goto :error
+
+.nuget\NuGet.exe push _deploy\ProductionStackTrace.%VERSION%.nupkg
+if errorlevel 1 goto :error
+.nuget\NuGet.exe push _deploy\ProductionStackTrace.Analyze.%VERSION%.nupkg
+if errorlevel 1 goto :error
+.nuget\NuGet.exe push _deploy\ProductionStackTrace.Analyze.Console.%VERSION%.nupkg
+if errorlevel 1 goto :error
+exit /b
+
+:uncommited
+echo There are uncommited changes! Please commit first.
+exit /b 1
+
+:error
+echo Something wrong, aborted
+exit /b 1
